cc62ce4c67445e7dd70ace054ec733ae3ae4e68a
AudreyFelicio <audreyanwar@gmail.com>
2021-03-14 15:54:17 +0800

    Create sign up page and view model

---
+++ Trippy/Trippy/SignUpView.swift
@@ -0,0 +1,31 @@
+//
+//  SignUpView.swift
+//  Trippy
+//
+//  Created by Audrey Felicio Anwar on 14/3/21.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @ObservedObject var signUpViewModel = SignUpViewModel()
+
+    var body: some View {
+        VStack {
+            Text("CREATE ACCOUNT")
+            TextField("Email", text: $signUpViewModel.email)
+            TextField("Password", text: $signUpViewModel.password)
+            TextField("Confirm Password", text: $signUpViewModel.confirmPassword)
+            Button("Sign Up") {
+                self.signUpViewModel.signUp()
+            }
+        }
+        .padding()
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
---
+++ Trippy/Trippy/SignUpViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewModel.swift
+//  Trippy
+//
+//  Created by Audrey Felicio Anwar on 14/3/21.
+//
+
+import SwiftUI
+import Combine
+
+final class SignUpViewModel: ObservableObject, Identifiable {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var confirmPassword = ""
+
+    func signUp() {
+        if password == confirmPassword {
+            print("Success!")
+        }
+    }
+}
