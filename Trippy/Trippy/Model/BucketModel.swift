60d561895b67c013ac7bd0b82c7cbc32f04ff1c5
Chan Qin Liang <qlchan24@gmail.com>
2021-03-17 19:38:52 +0800

    minor fix

--- Trippy/Trippy/Model/BucketModel.swift
+++ Trippy/Trippy/Model/BucketModel.swift
@@ -17,22 +17,14 @@ class BucketModel : ObservableObject {
 
     func addBucketItem(bucketItem: BucketItem) throws {
         try storage.addBucketItem(bucketItem: bucketItem)
-        bucketItems.append(bucketItem)
     }
 
     func removeBucketItem(bucketItem: BucketItem) {
         storage.removeBucketItem(bucketItem: bucketItem)
-        bucketItems.removeAll{$0.id == bucketItem.id}
     }
 
     func updateBucketItem(bucketItem: BucketItem) throws {
         try storage.updateBucketItem(bucketItem: bucketItem)
-        bucketItems = bucketItems.map{item in
-            if item.id == bucketItem.id {
-                return bucketItem
-            }
-            return item
-        }
     }
 }
 
