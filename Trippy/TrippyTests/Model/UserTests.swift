c13b3f1389ea4c7235444216f07b79f007940284
AudreyFelicio <audreyanwar@gmail.com>
2021-03-21 17:38:22 +0800

    Add user tests

---
+++ Trippy/TrippyTests/Model/UserTests.swift
@@ -0,0 +1,19 @@
+//
+//  UserTests.swift
+//  TrippyTests
+//
+//  Created by Audrey Felicio Anwar on 21/3/21.
+//
+
+import XCTest
+@testable import Trippy
+
+class UserTests: XCTestCase {
+    func testInit() {
+        let user = User(id: "3217", email: "trippy@3217.com", username: "Trippy", friendsId: [])
+        XCTAssertEqual(user.id, "3217")
+        XCTAssertEqual(user.email, "trippy@3217.com")
+        XCTAssertEqual(user.username, "Trippy")
+        XCTAssertEqual(user.friendsId, [])
+    }
+}
